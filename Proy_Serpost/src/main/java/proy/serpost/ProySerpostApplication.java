1ca299aea4455b89b69b88c4738a69d971018ab0
Frank <dablacksuite@gmail.com>
2022-04-21 12:37:37 -0500

    proyecto base

---
+++ Proy_Serpost/src/main/java/proy/serpost/ProySerpostApplication.java
@@ -0,0 +1,16 @@
+package proy.serpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProySerpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProySerpostApplication.class, args);
+		System.out.println("dale marmota!!!!");
+		System.out.println("ya casi se logra");
+		System.out.println("Termino de cargar la app >:V");
+	}
+
+}
